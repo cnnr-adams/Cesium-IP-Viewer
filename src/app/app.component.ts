b984524b0b5e216061346d8d91e778dd983b1f52
Connor Adams <cnnr252@gmail.com>
2018-08-24 20:39:38 -0400

    Angular + Cesium Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Request, Http, RequestMethod } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Cesium-IP-Viewer';
+  constructor(private http: Http) { }
+  ngOnInit() {
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+
+      imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
+        url: '//services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
+      })
+    });
+    const pointCollection = viewer.scene.primitives.add(new Cesium.PointPrimitiveCollection());
+    // console.log('here');
+    this.http.request(new Request({ method: RequestMethod.Get, url: "http://192.168.0.29:3000" })).subscribe((body) => {
+      //   console.log(body.json());
+      body.json().forEach(group => {
+        //  console.log(group.lon, group.lat);
+        pointCollection.add({
+          position: new Cesium.Cartesian3.fromDegrees(group.lon, group.lat),
+          color: new Cesium.Color(Math.random(), Math.random(), Math.random()),
+          pixelSize: 2,
+          scaleByDistance: new Cesium.NearFarScalar(0, 5, 3.5e7, 1)
+        });
+      });
+    });
+  }
 }
---
+++ src/app/remove-cesium-stuff.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appRemoveCesiumStuff]'
+})
+export class RemoveCesiumStuffDirective implements OnInit {
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    console.log(this.elementRef.nativeElement.firstChild.firstChild, this.elementRef.nativeElement.firstChild.lastChild)
+    while (this.elementRef.nativeElement.firstChild.firstChild !== this.elementRef.nativeElement.firstChild.lastChild) {
+      this.elementRef.nativeElement.firstChild.removeChild(this.elementRef.nativeElement.firstChild.lastChild)
+    }
+    Cesium.Fullscreen.requestFullScreen(this.elementRef);
+  }
+}
